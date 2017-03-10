4f895eea0b98df919a7dec621c2053ea9d876235
Mary Ruth Ngo <mngo@wellesley.edu>
2017-03-10 12:32:37 -0500

    restructured to pass all of scotts tests except XSS attack

--- assignment/wmdb-search.php
+++ assignment/wmdb-search.php
@@ -12,10 +12,10 @@
 	<!-- form at the top of the page -->
 	<div class='container'>
 	  <form action='#' method='get' class='row'>
-	    <select class="form-control offset-sm-2 col-sm-1" name='type'>
+	    <select class="form-control offset-sm-2 col-sm-1" name='tables'>
 	      <option value='both'>both</option>
-	      <option value='name'>name</option>
-	      <option value='title'>movie title</option>
+	      <option value='names'>name</option>
+	      <option value='titles'>movie title</option>
 	    </select>
 	    <input type="text" class="form-control col-sm-6" name='sought'>
 	    <button type="submit" class="btn btn-primary col-sm-1">Submit</button>
@@ -34,56 +34,66 @@
 	// these will be populated depending on the request that comes in
 	$nm = isset($_REQUEST['nm']) ? $_REQUEST['nm']: -1;
 	$tt = isset($_REQUEST['tt']) ? $_REQUEST['tt']: -1;
-	$type = isset($_REQUEST['type']) ? $_REQUEST['type']: -1;
+	$type = isset($_REQUEST['tables']) ? $_REQUEST['tables']: -1;
 	$request = isset($_REQUEST['sought']) ? $_REQUEST['sought']: -1;
 
 
 	//------------------ PREPARED QUERY TEMPLATES -----------------
 
 	// given user search - returns name, birthday of all matches
-	$sql_name = "SELECT distinct name,birthdate,nm from person  
+	$sql_several_names = "SELECT distinct name,birthdate,nm from person  
 				 where person.name like concat('%',?,'%');";
 
 	// given nm - returns the name and birthdate of that person.
-	$sql_single_name = "SELECT name,birthdate from person
+	$sql_single_name = "SELECT name,birthdate,nm from person
 						where person.nm=?;";
 
 	// given nm - returns names of movies from particular actor
-	$sql_name_movies = "SELECT title,movie.tt as tt from person,credit,movie
+	$sql_single_name_movies = "SELECT title,movie.tt as tt from person,credit,movie
 						where person.nm=?
 						and person.nm=credit.nm 
 						and credit.tt=movie.tt;";
 
-	// given user search - returns number of matches
-	$sql_name_count = "SELECT distinct count(*) from person  
-				 	   where person.name like concat('%',?,'%');";
-
 	// given user search - returns movies (title and release date)
-	$sql_title = "SELECT distinct title,`release`,tt from movie
+	$sql_several_titles = "SELECT distinct title,`release`,tt from movie
 				  where movie.title like concat('%',?,'%');";
 	
 	// given tt - returns the title, release dat
-	$sql_single_title = "SELECT title, `release` from
+	$sql_single_title = "SELECT title,`release`,tt from
 						movie where movie.tt=?;";
+
+	// given tt - returns the director of that movie
+	$sql_single_title_director = "SELECT name from movie,person
+						where movie.tt=? and movie.director=person.nm;";
+
+	// given tt - returns the actors in that movie
+	$sql_single_title_actors = "SELECT distinct name,birthdate,person.nm as nm from
+						 movie,credit,person
+						 where movie.tt=?
+						 and movie.tt=credit.tt
+						 and person.nm=credit.nm;";
 
 	// given user search - returns the number of movies that match
 	$sql_title_count = "SELECT distinct count(*) from movie
 				  where movie.title like concat('%',?,'%');";
 
-	// given tt - returns the director of that movie
-	$sql_title_director = "SELECT name from movie,person
-						where movie.tt=? and movie.director=person.nm;";
-
-	// given tt - returns the actors in that movie
-	$sql_title_actors = "SELECT distinct name,birthdate,person.nm as nm from
-						 movie,credit,person
-						 where movie.tt=?
-						 and movie.tt=credit.tt
-						 and person.nm=credit.nm;";
+	// given user search - returns number of matches
+	$sql_name_count = "SELECT distinct count(*) from person  
+				 	   where person.name like concat('%',?,'%');";
 	
 	//------------------ END PREPARED QUERY TEMPLATES -----------------
 
     //----------------- FUNCTIONS -----------------
+	// php function to just return the count of matches for a particular query
+	function get_count($sql_count) {
+		global $dbh, $request;
+		$counter = prepared_query($dbh, $sql_count, array($request));
+		$count = 0;
+		while ($row = $counter->fetchRow(MDB2_FETCHMODE_ASSOC)) {
+			$count = $row['count(*)'];
+		}
+		return $count;
+	}
 
 	// php function for echo-ing all of the names matched by a user search
 	function display_all_names($resultset) {
@@ -98,17 +108,19 @@
 	// php function for echo-ing detailed information from a single person
 	// based on a hyperlink that was clicked, providing an id for that person (nm)
 	function display_single_name($resultset) {
-		global $dbh, $self, $sql_name_movies, $nm, $tt;
+		global $dbh, $self, $nm;
+		// echo "$nm";
  		echo "<div class = \"single_entry\">";
  		while($row = $resultset->fetchRow(MDB2_FETCHMODE_ASSOC)) {
-	    	echo "<h3>${row['name']} \n was born on ${row['birthdate']}</h3>";
+ 			$nm = $row['nm'];
+	    	echo "<h3>${row['name']}</h3> \n <h6>born on ${row['birthdate']}</h6>";
 	    }
-	    display_filmography_of_actor();
+	    display_filmography_of_single_name();
 	}
 
-	function display_filmography_of_actor() {
-		global $dbh, $sql_name_movies, $nm;
- 		$movies = prepared_query($dbh,$sql_name_movies,array($nm));
+	function display_filmography_of_single_name() {
+		global $dbh, $sql_single_name_movies, $nm, $self;
+ 		$movies = prepared_query($dbh,$sql_single_name_movies,array($nm));
 	    echo "<h5>Filmography:</h5><ul> ";
 	    while($row = $movies->fetchRow(MDB2_FETCHMODE_ASSOC)) {
 	    	echo "<li><a href='$self?tt=${row['tt']}'>${row['title']}</a>";
@@ -130,10 +142,11 @@
 	// php function for echo-ing detailed information for a particular movie
 	// based on a hyperlink clicked which provided an id for that movie (tt)
 	function display_single_movie($resultset) {
-		global $dbh, $self, $sql_title_actors, $sql_title_director, $nm, $tt;
+		global $dbh, $self, $tt;
 
 		echo "<div class = \"single_entry\">";
 		while($row = $resultset->fetchRow(MDB2_FETCHMODE_ASSOC)) {
+			$tt = $row['tt'];
 		    echo "<h3>${row['title']}  (${row['release']}) </h3>";
 		}
 		display_director_of_movie();
@@ -141,18 +154,18 @@
 	}
 
 	function display_director_of_movie() {
-		global $dbh,$sql_title_director, $tt;
+		global $dbh,$sql_single_title_director, $tt;
 		// display director
-		$director = prepared_query($dbh,$sql_title_director,array($tt));
+		$director = prepared_query($dbh,$sql_single_title_director,array($tt));
 		while ($row = $director->fetchRow(MDB2_FETCHMODE_ASSOC)) {
 		    echo "<h6>directed by: ${row['name']}</h6>";
 		}
 	}
 
 	function display_actors_of_movie() {
-		global $dbh, $sql_title_actors, $tt;
+		global $dbh, $self, $sql_single_title_actors, $tt;
 		// display hyperlinks for all actors that database says acted in that movie
-		$actors = prepared_query($dbh,$sql_title_actors,array($tt));
+		$actors = prepared_query($dbh,$sql_single_title_actors,array($tt));
 		echo "<h5>Cast:</h5><ul> ";
 	    while($row = $actors->fetchRow(MDB2_FETCHMODE_ASSOC)) {
 	    	echo "<li><a href='$self?nm=${row['nm']}'>${row['name']}</a>";
@@ -160,16 +173,6 @@
 		echo "</ul></div>";
 	}
 
-	// php function to just return the count of matches for a particular query
-	function get_count($sql_count) {
-		global $dbh, $request;
-		$counter = prepared_query($dbh, $sql_count, array($request));
-		$count = 0;
-		while ($row = $counter->fetchRow(MDB2_FETCHMODE_ASSOC)) {
-			$count = $row['count(*)'];
-		}
-		return $count;
-	}
 	// ------------------------ END FUNCTIONS -------------------------
 	
 	// FIRST: User-inputted form
@@ -178,15 +181,15 @@
         $both_name_count = 0;
         $both_title_count = 0;
 
-	    if ($type == 'name' or $type == 'both') {
-		    $resultset_name = prepared_query($dbh,$sql_name,array($request));
+	    if ($type == 'names' or $type == 'both') {
+		    $resultset_name = prepared_query($dbh,$sql_several_names,array($request));
 		    $count_name = get_count($sql_name_count);
 
 		    if ($count_name > 1) {
 		      display_all_names($resultset_name);
 	 	    } else if ($count_name == 1) {
 	               if ($type != 'both') {
-	                   display_single_name($resultset_name);
+	                    display_single_name($resultset_name);
 	               } else {
 	                   display_all_names($resultset_name);
 	               }
@@ -195,20 +198,20 @@
 		    }
 	    }
 
-	    if ($type == 'title' or $type == 'both') {
-		    $resultset_title = prepared_query($dbh,$sql_title,array($request));
+	    if ($type == 'titles' or $type == 'both') {
+		    $resultset_titles = prepared_query($dbh,$sql_several_titles,array($request));
 		    $count_title = get_count($sql_title_count);
 
 		    if ($count_title > 1) {
 
-		    display_all_movies($resultset_title);
+		    display_all_movies($resultset_titles);
 
 		    } else if ($count_title == 1) {
 	                if ($type != 'both') {
-	                   display_single_movie($resultset_title);
-	               } else {
-	                   display_single_movie($resultset_title);
-	               }
+	                    display_single_movie($resultset_titles);
+	                } else {
+						display_all_movies($resultset_titles);
+	                }
 		    } else if ($type != 'both') {
 		    echo "No movies match \"$request\" :(";
 	    	}
