7e7a72b4d3b5b88a6339c95d046fd8d5c09ef523
mhejmadi <mhejmadi@wellesley.edu>
2017-03-09 20:50:11 -0500

    please

--- assignment/wmdbsearch.php
+++ assignment/wmdbsearch.php
@@ -1,41 +1,42 @@
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>WMDB Search></title>
-		<link rel='stylesheet' type='text/css' href='css/wmdb-search.css'>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
-		integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-	</head>
-	<style>
-		.row {
-			margin-top: 10px;
-		}
+  <head>
+    <meta charset="utf-8">
+    <title>WMDB Search></title>
+    <link rel='stylesheet' type='text/css' href="styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
+	  integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  </head>
+  <style>
+    .row {
+    margin-top: 10px;
+    }
 
-		li {
+    li {
 
-		}
-	</style>
-	<body>
+    }
+  </style>
+  <body>
 
-	<div class='container'>
-		<form action='#' method='get' class='row'>
-		   <select class="form-control offset-sm-2 col-sm-1" name='type'>
-		      <option value='name'>name</option>
-		      <option value='title'>movie title</option>
-		      <option value='both'>both</option>
-		    </select>
-		    <input type="text" class="form-control col-sm-6" name='sought'>
-		    <button type="submit" class="btn btn-primary col-sm-1">Submit</button>
-		</form>
-		<div class='row' id='singleresult'>
-			<div class='offset-lg-3 col-lg-6'>
-				<h4 id='name_or_title'></h4>
-				<h6 id='date'></h6>
-			</div>
-		</div>
-	</div>
+<!-- Want to fix this because it doesn't shrink with page. Meh. -->
+<div class='container'>
+  <form action='#' method='get' class='row'>
+    <select class="form-control offset-sm-2 col-sm-1" name='type'>
+      <option value='both'>both</option>
+      <option value='name'>name</option>
+      <option value='title'>movie title</option>
+    </select>
+    <input type="text" class="form-control col-sm-6" name='sought'>
+    <button type="submit" class="btn btn-primary col-sm-1">Submit</button>
+  </form>
+  <div class='row' id='singleresult'>
+    <div class='offset-lg-3 col-lg-6'>
+      <h4 id='name_or_title'></h4>
+      <h6 id='date'></h6>
+    </div>
+  </div>
+</div>
 
-	<?php
+<?php
 
 	require_once("/home/cs304/public_html/php/DB-functions.php");
 	require_once('mngo_mhejmadi_dsn.inc');
@@ -45,77 +46,82 @@
 	#------------------ PREPARED QUERY TEMPLATES -----------------
 
 	$sql_name = "SELECT distinct name,birthdate from person
-				 where person.name like concat('%',?,'%');";
+				 where person.name like ?;";
 
 	$sql_name_movies = "SELECT distinct title from person,credit,movie
-						where person.name like concat('%',?,'%')
-						and person.nm=credit.nm and credit.tt=movie.tt;";
+			    where person.name like concat('%',?,'%')
+			    and person.nm=credit.nm and credit.tt=movie.tt;";
 
 	$sql_name_count = "SELECT distinct count(*) from person
-				 	   where person.name like concat('%',?,'%');";
+			   where person.name like concat('%',?,'%');";
 
-	$sql_title = "SELECT distinct title,`release`,name from
-				  movie,person
-				  where movie.title like concat('%',?,'%')
-				  and movie.director=person.nm;";
+        $sql_title = "SELECT distinct title,`release`,name from
+		      movie,person
+		      where movie.title like concat('%',?,'%')
+		      and  movie.director=person.nm;";
 
 	$sql_title_count = "SELECT distinct count(*) from
 				  movie,person as director
 				  where movie.title like concat('%',?,'%')
-				  and movie.director=director.nm;";
+				  and movie.director=director.nm;;";
 
 	$sql_title_actors = "SELECT distinct name from
-			  movie,credit,person
+			  movie,credit, person
 			  where movie.title like concat('%',?,'%')
 			  and movie.tt=credit.tt
 			  and person.nm=credit.nm;";
 
-	$self = $_SERVER['PHP_SELF'];
 	$type = isset($_REQUEST['type']) ? $_REQUEST['type']: -1;
 	$request = isset($_REQUEST['sought']) ? $_REQUEST['sought']: -1;
+	$self = $_SERVER['PHP_SELF'];
 
-	//----------------- FUNCTIONS -----------------
+   //----------------- FUNCTIONS -----------------
+
 	function write_name_several($resultset) {
-		global $self;
+		global $self, $request;
+	      echo "Here are the people that match \"$request\":\n<ul>";
 		while($row = $resultset->fetchRow(MDB2_FETCHMODE_ASSOC)) {
-		    echo "<li><a href='$self?type=name&sought=${row['name']}'>${row['name']}  ${row['birthdate']}</a>";
+		    echo "<li class = \"multi_entries\"><a href='$self?type=name&sought=${row['name']}'>${row['name']} ${row['birthdate']}</a>";
 		}
+	    echo "</ul>\n";
 	}
 
 	function write_name_single($resultset) {
-		global $dbh, $sql_name_movies;
+		global $dbh, $self, $sql_name_movies;
  		$movies = prepared_query($dbh,$sql_name_movies,array($_REQUEST['sought']));
- 		//make fancier later
+ 		echo "<div class = \"single_entry\">";
  		while($row = $resultset->fetchRow(MDB2_FETCHMODE_ASSOC)) {
 	    	echo "<h3>${row['name']} \n was born on ${row['birthdate']}</h3>";
 	    }
-
-	    echo "<ul>Filmography: ";
+	    echo "<h5>Filmography:</h5><ul> ";
 	    while($row = $movies->fetchRow(MDB2_FETCHMODE_ASSOC)) {
-	    	echo "<li>${row['title']}";
+	    	echo "<li><a href='$self?type=title&sought=${row['title']}'>${row['title']}</a>";
 	    }
+		echo "</ul></div>";
 	}
 
 	function write_title_several($resultset) {
-		global $self;
+		global $self, $request;
+	    echo "Here are the movies that match \"$request\":\n<ul>";
 		while($row = $resultset->fetchRow(MDB2_FETCHMODE_ASSOC)) {
-		    echo "\t<li><a href='$self?type=title&sought=${row['title']}'>${row['title']}  (${row['release']})</a>\n";
+		    echo "\t<li class = \"multi_entries\"><a href='$self?type=title&sought=${row['title']}'>${row['title']}  (${row['release']})</a>\n";
 		 }
+	    echo "</ul>\n";
 	}
 
 	function write_title_single($resultset) {
-		global $dbh, $sql_title_actors;
+		global $dbh, $self, $sql_title_actors;
 		$actors = prepared_query($dbh,$sql_title_actors,array($_REQUEST['sought']));
-
+		echo "<div class = \"single_entry\">";
 		while($row = $resultset->fetchRow(MDB2_FETCHMODE_ASSOC)) {
 		    echo "<h3>${row['title']}  (${row['release']}) </h3>";
 		    echo "<h6>directed by: ${row['name']}</h6>";
 		}
-
-		echo "<ul>Cast: ";
+		echo "<h5>Cast:</h5><ul> ";
 	    while($row = $actors->fetchRow(MDB2_FETCHMODE_ASSOC)) {
-	    	echo "<li>${row['name']}";
+	    	echo "<li><a href='$self?type=name&sought=${row['name']}'>${row['name']}</a>";
 	    }
+		echo "</ul></div>";
 	}
 
 	function get_count($sql_count) {
@@ -133,91 +139,49 @@
 
 	# ------------------------- LOGIC TREE ----------------------------
 	if ($type != -1) {
-		if ($type == 'name' or $type == 'both') {
-			$resultset_name = prepared_query($dbh,$sql_name,array($request));
-			$count_name = get_count($sql_name_count);
+          $both_name_count = 0;
+          $both_title_count = 0;
+	  if ($type == 'name' or $type == 'both') {
+	    $resultset_name = prepared_query($dbh,$sql_name,array(concat('%',$request,'%')));
+	    $count_name = get_count($sql_name_count);
 
-			if ($count_name > 1) {
-				echo "Here are the people that match $request:\n";
-				write_name_several($resultset_name);
-		 	} else if ($count_name == 1) {
-		 		// echo "Here is the person that matches $request:\n";
-		 		write_name_single($resultset_name);
-		 	} else if ($type != 'both') {
-		 		echo "No names match $request :(";
-		 	}
-		}
+	    if ($count_name > 1) {
+	      write_name_several($resultset_name);
+ 	    } else if ($count_name == 1) {
+               if ($type != 'both') {
+                   write_name_single($resultset_name);
+               } else {
+                   write_name_several($resultset_name);
+               }
+	    } else if ($type != 'both') {
+	    echo "No names match \"$request\" :(";
+	    }
+	    }
 
-		if ($type == 'title' or $type == 'both') {
-			$resultset_title = prepared_query($dbh,$sql_title,array($request));
-			$count_title = get_count($sql_title_count);
+	    if ($type == 'title' or $type == 'both') {
+	    $resultset_title = prepared_query($dbh,$sql_title,array($request));
+	    $count_title = get_count($sql_title_count);
 
-			if ($count_title > 1) {
-				echo "Here are the movies that match $request:\n";
-				write_title_several($resultset_title);
-			} else if ($count_title == 1) {
-				// echo "Here is the movie that matches $request:\n";
-				write_title_single($resultset_title);
-			} else if ($type != 'both') {
-				echo "No movies match $request :(";
-			}
-		}
+	    if ($count_title > 1) {
 
-		if ($type == 'both' and $count_name == 0 and $count_title == 0) {
-			echo "<h2>Literally nothing matched $request :(</h2>";
-		}
-	}
+	    write_title_several($resultset_title);
 
+	    } else if ($count_title == 1) {
+                if ($type != 'both') {
+                   write_title_single($resultset_title);
+               } else {
+                   write_title_single($resultset_title);
+               }
+	    } else if ($type != 'both') {
+	    echo "No movies match \"$request\" :(";
+	    }
+	    }
 
-
-
-	// } else if ($type == 'both') {
-	// 	$name_resultset = prepared_query($dbh,$sql_name,array($_REQUEST['sought']));
-	// 	$title_resultset = prepared_query($dbh,$sql_title,array($_REQUEST['sought']));
-
-	// 	$name_count_set = prepared_query($dbh,$sql_name_count,array($_REQUEST['sought']));
-	// 	$name_count = 0;
-	// 	while ($row = $name_count_set->fetchRow(MDB2_FETCHMODE_ASSOC)) {
-	// 		$name_count = $row['count(*)'];
-	// 	}
-
-	// 	$title_count_set = prepared_query($dbh,$sql_title_count,array($_REQUEST['sought']));
-	// 	$title_count = 0;
-	// 	while ($row = $title_count_set->fetchRow(MDB2_FETCHMODE_ASSOC)) {
-	// 		$title_count = $row['count(*)'];
-	// 	}
-
-
-	// 	if ($name_count > 0) {
-	// 		echo "<h3>$name_count Names matched</h3>";
-	// 		if ($name_count > 1) {
-	// 			write_name_several($name_resultset);
-	// 		} else {
-	// 			write_name_single($name_resultset);
-	// 		}
-	// 	}
-
-	// 	if ($title_count > 0) {
-	// 		echo "<h3>$title_count Movies matched</h3>";
-	// 		if ($title_count > 1) {
-	// 			write_title_several($title_resultset);
-	// 		} else {
-	// 			write_title_single($title_resultset);
-	// 		}
-	// 	}
-
-	// 	if (($title_count == 0) && ($name_count == 0)) {
-	// 		$request = $_REQUEST['sought'];
-	// 		echo "<h3> No Results for $request</h3>";
-	// 	}
-
-	// }
-	// The resultset object has a fetchRow method that can give us back the
-	// next row of the resultset, stored in an associative array with
-	// keys named for the columns we asked for (nm,name).
-
+	    if ($type == 'both'and $count_name == 0 and $count_title == 0) {
+                    echo "<h2>Literally nothing matched \"$request\" :(</h2>";
+	    }
+	    }
 	?>
-	</ol>
 
 	</body>
 	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"
